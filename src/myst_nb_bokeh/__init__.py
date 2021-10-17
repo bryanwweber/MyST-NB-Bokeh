d300676bb93ba34cf48076fcd52083f1596cfe68
Bryan W. Weber <bryan.w.weber@gmail.com>
2021-10-17 19:50:41 -0400

    Release 0.5.2

--- src/myst_nb_bokeh/__init__.py
+++ src/myst_nb_bokeh/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 __author__ = """Bryan Weber"""
 __email__ = "bryan.w.weber@gmail.com"
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 import json
 from textwrap import dedent
