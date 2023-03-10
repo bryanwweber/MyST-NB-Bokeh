2d3b7cd317c67ed167d58101cd4d9272893bf831
Bryan Weber <bryan.w.weber@gmail.com>
2023-03-09 19:42:40 -0500

    Bump to 1.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -4,15 +4,7 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import myst_nb_bokeh
 
 # -- Project information -----------------------------------------------------
 
@@ -21,7 +13,7 @@ copyright = "2022, Bryan Weber"
 author = "Bryan Weber"
 
 # The full version, including alpha/beta/rc tags
-release = "0.5.3"
+release = myst_nb_bokeh.__version__
 
 
 # -- General configuration ---------------------------------------------------
